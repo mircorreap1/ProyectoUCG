68865613cb7ec044c51d49c8dcff2ed8e27c3edd
mircorreap1 <mcorreap@unemi.edu.ec>
2026-05-16 10:05:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ interes= st.number_input("Ingrese el interes", min_value=0.0, max_value=1.0, val
 anios= st.number_input ("Ingrese el número de años del préstamo", value=1)
 numero_pagos= st.number_input ("Ingrese el número de pagos anuales", value=12)
 
-cuota = lf.cuota_prestamo (1000, 0.10, 5,12)
+cuota = lf.cuota_prestamo (monto, interes, anios,numero_pagos)
 st.write(cuota)
