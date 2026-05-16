19cb82acb07bd7571b081d60097d35310f1b76f2
mircorreap1 <mcorreap@unemi.edu.ec>
2026-05-16 10:02:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ for uploaded_file in uploaded_files:
     df = pd.read_csv(uploaded_file)
     st.write(df)
 
+monto= st.number_input("Ingrese monto:" )
+interes= st.number_input("Ingrese el interes")
+anios= st.number_input ("Ingrese el número de años del préstamo")
+numero_pagos= st.number_input ("Ingrese el número de pagos anuales")
+
 cuota = lf.cuota_prestamo (1000, 0.10, 5,12)
 st.write(cuota)
