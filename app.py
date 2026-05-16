819101da35cd9df3751b81df3ec01e2a13bf3b74
mircorreap1 <mcorreap@unemi.edu.ec>
2026-05-16 09:53:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import pandas as pd
 import streamlit as st
 
 st.title("Proyecto Final UCG")
 st.sidebar.title("Parámetros")
 st.sidebar.image("python_logo.png")
+
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
