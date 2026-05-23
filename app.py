1479facd29da406d0f08add8f0caa50b180166ae
mircorreap1 <mcorreap@unemi.edu.ec>
2026-05-23 09:59:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -31,17 +31,17 @@ else:
 
 
 
-uploaded_files = st.file_uploader(
-    "Upload data", accept_multiple_files=True, type="csv"
-)
-for uploaded_file in uploaded_files:
-    df = pd.read_csv(uploaded_file)
-    st.write(df)
+#uploaded_files = st.file_uploader(
+#    "Upload data", accept_multiple_files=True, type="csv"
+#)
+#for uploaded_file in uploaded_files:
+#    df = pd.read_csv(uploaded_file)
+#    st.write(df)
 
-monto= st.number_input("Ingrese monto:", min_value=0, max_value=1000, value=1000)
-interes= st.number_input("Ingrese el interes", min_value=0.0, max_value=1.0, value=0.10)
-anios= st.number_input ("Ingrese el número de años del préstamo", value=1)
-numero_pagos= st.number_input ("Ingrese el número de pagos anuales", value=12)
+#monto= st.number_input("Ingrese monto:", min_value=0, max_value=1000, value=1000)
+#interes= st.number_input("Ingrese el interes", min_value=0.0, max_value=1.0, value=0.10)
+#anios= st.number_input ("Ingrese el número de años del préstamo", value=1)
+#numero_pagos= st.number_input ("Ingrese el número de pagos anuales", value=12)
 
 cuota = lf.cuota_prestamo (monto, interes, anios,numero_pagos)
 st.write(cuota)
