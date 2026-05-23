d493109b84b01c8ee1045d284c292a8d6fac5fb0
mircorreap1 <mcorreap@unemi.edu.ec>
2026-05-23 10:00:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ if modulo == "Módulo 1":
         st.write(df)
 
 elif modulo == "Módulo 2":
+   
     monto = st.number_input("Ingrese el monto:", min_value = 0 , max_value = 10000, value=1000)
     interes = st.number_input("Ingrese el interes:",min_value = 0.0 , max_value = 1.0, value=0.10)
     anios = st.number_input("Ingrese el número de años del prestamo:",value=1)
@@ -43,5 +44,5 @@ else:
 #anios= st.number_input ("Ingrese el número de años del préstamo", value=1)
 #numero_pagos= st.number_input ("Ingrese el número de pagos anuales", value=12)
 
-cuota = lf.cuota_prestamo (monto, interes, anios,numero_pagos)
-st.write(cuota)
+#cuota = lf.cuota_prestamo (monto, interes, anios,numero_pagos)
+#st.write(cuota)
