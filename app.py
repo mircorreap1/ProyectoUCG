0a02b138dc2f19985710eed16faf8d65007f72fd
mircorreap1 <mcorreap@unemi.edu.ec>
2026-05-23 09:53:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,27 @@ st.sidebar.image("python_logo.png")
 
 modulo = st.selectbox ("sleccione un módulo", ["Módulo 1", "Módulo 2", "Módulo 3"])
 
-if modulo == "Modulo 1":
-    upload_files= st.file_uploader(
-        "Upload data", accep_multiple_fiales, type="csv"
+if modulo == "Módulo 1":
+
+    uploaded_files = st.file_uploader(
+        "Upload data", accept_multiple_files=True, type="csv"
     )
+    for uploaded_file in uploaded_files:
+        df = pd.read_csv(uploaded_file)
+        st.write(df)
+
+elif modulo == "Módulo 2":
+    monto = st.number_input("Ingrese el monto:", min_value = 0 , max_value = 10000, value=1000)
+    interes = st.number_input("Ingrese el interes:",min_value = 0.0 , max_value = 1.0, value=0.10)
+    anios = st.number_input("Ingrese el número de años del prestamo:",value=1)
+    numero_pagos = st.number_input("Ingrese el número pagos anuales:" , value = 12)
+    
+    cuota = lf.cuota_prestamo(monto, interes,anios,numero_pagos)
+    st.write("Su cuota mensual es: ",cuota)
+
+else:
+    st.write("Se encuentra en el módulo 3")
+
 
 
 uploaded_files = st.file_uploader(
